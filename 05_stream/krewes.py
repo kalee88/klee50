8c66a633124cbdcc7f99f854b85bcb6da95809bc
Kyle Lee <117378813+kalee88@users.noreply.github.com>
2024-09-18 11:43:24 -0400

    Added comments

--- 05_stream/krewes.py
+++ 05_stream/krewes.py
@@ -8,15 +8,15 @@
 import random
 with open("krewes.txt", "r") as file:
     f = file.read()
-tuples = f.split("@@@")
-devos = []
+tuples = f.split("@@@") #Split into each devo
+devos = [] 
 for tuple in tuples:
-    info = tuple.split("$$$")
-    if len(info) == 3:
+    info = tuple.split("$$$") 
+    if len(info) == 3: 
         period, devo, ducky = info
-        devos.append({'period': period, 'devo': devo, 'ducky': ducky})
-if devos:
-    random_devo = random.choice(devos)
+        devos.append({'period': period, 'devo': devo, 'ducky': ducky}) #Add dictionary
+if devos: #If there are anything in devos
+    random_devo = random.choice(devos) #Get a random dictionary in devo
     print(f"Devo: {random_devo['devo']}, Period: {random_devo['period']}, Ducky: {random_devo['ducky']}")
 else:
         print("No devos found.")
