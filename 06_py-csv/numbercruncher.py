fb9d78b2108dd0eb59b14c85d562bf4a029b1b65
Kyle Lee <117378813+kalee88@users.noreply.github.com>
2024-09-20 11:05:35 -0400

    Add files via upload

---
+++ 06_py-csv/numbercruncher.py
@@ -0,0 +1,27 @@
+#Kyle Lee
+#50_Ls (Kyle, Kevin)
+#SoftDev
+#K7 -- Reading csv Files
+#2024-09-18
+#time spent: .75
+
+import random
+import csv
+def Program(): 
+    with open('occupations.csv', "r") as file: #Don't have to close
+        csvFile = csv.reader(file)
+        info = {}
+        count = 0
+        rando_number = random.randint(0,998)
+        total = 0
+        for lines in csvFile:
+            if not(count == 0):
+                if (total > rando_number):
+                    print(lines[0])
+                    return(lines[0])
+                else:
+                    total += (float(lines[1])*10)
+                info.update({lines[0]:float(lines[1])}) #Add to dictionary
+            count = count + 1
+        info.popitem()
+Program()
