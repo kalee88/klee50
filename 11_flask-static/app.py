0f7bbbaf07238b68550500ca3a0369869b911adb
Kyle Lee <klee50@stuy.edu>
2024-09-27 07:22:34 -0400

    k11 stuff

--- 11_flask-static/app.py
+++ 11_flask-static/app.py
@@ -7,26 +7,22 @@
 import random
 from flask import Flask
 app = Flask(__name__)
-'''
+
 @app.route("/")
 def hello_world():
     print("the __name__ of this module is... ")
     print(__name__)
     return "No hablo queso!"
 
-'''
-@app.route("/static/fixie.html")
+
+@app.route("/static/foo.html")
 def h():
-    x = '''
-<!DOCTYPE html>
-<html>
-	<body>
-		<p>K^3 : Kyle, Suhana, Vedent.</p>'''
     print("the __name__ of this module is... ")
     print(__name__)
-    return str(x)
+    return str(random.random())
 
 
 if __name__ == "__main__":  # true if this file NOT imported
     app.debug = True        # enable auto-reload upon code change
     app.run()
+
