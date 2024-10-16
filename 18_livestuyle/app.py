2ed1eaa1db0db160a8c5055a4ae26005d2409dd3
Kyle Lee <klee50@stuy.edu>
2024-10-16 11:42:21 -0400

    k18

---
+++ 18_livestuyle/app.py
@@ -0,0 +1,11 @@
+# K^3 : Kyle Lee, Suhana Kumar, Vedent Kothari
+# SoftDev
+# Oct 16th 2024
+# DEMO
+# basics of /static folder
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+
+
+app = Flask(__name__)    #create Flask object
