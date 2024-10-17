d06326a9ebcec215a2081b512f812865df600bae
Kyle Lee <klee50@stuy.edu>
2024-10-17 09:49:38 -0400

    k18

--- 18_livestuyle/app.py
+++ 18_livestuyle/app.py
@@ -9,3 +9,11 @@ from flask import render_template   #facilitate jinja templating
 
 
 app = Flask(__name__)    #create Flask object
+@app.route("/")
+def htmlrender():
+    
+    return render_template( 'index.html')
+
+if __name__ == "__main__":      # true if this file NOT imported
+    app.debug = True            # enable auto-reload upon code change
+    app.run()
